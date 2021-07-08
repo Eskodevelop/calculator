bdef5403b1e4d2b29e404af287d9f5c128c63ab9
Eskodevelop <esmirneretljak@gmail.com>
2021-07-08 10:45:27 +0200

    6/6

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './index.css';
+import React from 'react';
+import Calculator from './Component/Calculator';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <div >
+<Calculator />
+    </div>
+  );
+}
+
+export default App;
